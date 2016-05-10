fb2a503920ab88707ce14977533259ce717b85dc
pazeshun <pazeshun3684@gmail.com>
2016-05-10 15:51:06 +0900

    overwrite symlink if its destination is the same as new one

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,17 @@ DOT_DIRECTORY=$(cd $(dirname $0) && pwd)
 rename_dotfiles () {
   if [ -e ~/${1} ]
   then
-    mv -i ~/${1} ~/${1}.origin
-    echo "~/${1} is found, rename it to ~/${1}.origin"
+    echo "~/${1} is found"
+    # check destinations of symbolic links
+    dest=`readlink ~/${1}`
+    if [ "$dest" = "${DOT_DIRECTORY}/${2}" ]
+    then
+      rm ~/${1}
+      echo "remove ~/${1} to overwrite"
+    else
+      mv -i ~/${1} ~/${1}.origin
+      echo "rename ~/${1} to ~/${1}.origin"
+    fi
   fi
 }
 
@@ -19,6 +28,6 @@ rename_dotfiles () {
 for str in "${src_to_dest[@]}"
 do
   ary=(`echo ${str}`)
-  rename_dotfiles ${ary[1]}
+  rename_dotfiles ${ary[1]} ${ary[0]}
   ln -snv ${DOT_DIRECTORY}/${ary[0]} ~/${ary[1]}
 done
