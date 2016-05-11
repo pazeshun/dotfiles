d8c5f89f50840f8f1cd062b2feab4bda0cd516e2
pazeshun <pazeshun3684@gmail.com>
2016-05-11 14:56:48 +0900

    for baxter

--- src_to_dest.sh
+++ src_to_dest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 src_to_dest=(
-"bashrc .bashrc"
-"gitconfig .gitconfig"
-"vim .vim"
-"vimrc .vimrc"
-"bash_aliases .bash_aliases"
-"emacs.d .emacs.d"
+#"bashrc .bashrc"
+"gitconfig .gitconfig.pazeshun"
+"vim .vim.pazeshun"
+"vimrc .vimrc.pazeshun"
+#"bash_aliases .bash_aliases"
+#"emacs.d .emacs.d"
 "roscpp_code_format/.clang-format .clang-format"
-"percol.d .percol.d"
+#"percol.d .percol.d"
 )
